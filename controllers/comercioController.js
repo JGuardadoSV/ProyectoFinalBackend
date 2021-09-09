bce3b44f5681827779285f4abe0513ad4c4af3b5
JGuardadoSV <47751227+JGuardadoSV@users.noreply.github.com>
2021-09-08 20:34:47 -0600

    CRUD comercios find y save

--- controllers/comercioController.js
+++ controllers/comercioController.js
@@ -3,10 +3,17 @@ import comercio from "../models/comercio.js";
 
 const controlador={}
 
-controlador.listado= (req,res)=>{
+controlador.listado= async (req,res)=>{
     console.log("Ejecutando el FIND")
-    
-    const comercios= comercio.find()
+    const comercios= await comercio.find()
+    res.json(comercios)
+}
+
+controlador.registrar= async (req,res)=>{
+    const nuevocomercio = new comercio(req.body)
+    console.log(nuevocomercio)
+    await nuevocomercio.save();
+    res.send("Se creo nuevo comercio")
 }
 
 export default controlador
--- routes/comercio.routes.js
+++ routes/comercio.routes.js
@@ -3,6 +3,7 @@ import cController from "../controllers/comercioController.js"
 const ruta = Router();
 
 ruta.get("/listadoComercios",cController.listado)
+ruta.post("/registrar",cController.registrar)
 
 
 export default ruta
