98b840f0771e82c1a338acae8b7c6573670c4a0a
JGuardadoSV <47751227+JGuardadoSV@users.noreply.github.com>
2021-09-10 18:46:00 -0600

    crud de comercios listo

--- controllers/comercioController.js
+++ controllers/comercioController.js
@@ -9,11 +9,31 @@ controlador.listado= async (req,res)=>{
     res.json(comercios)
 }
 
+controlador.uno= async (req,res)=>{
+    console.log("Consulta individual")
+    const uncomercio= await comercio.findById(req.params.id)
+    res.json(uncomercio)
+}
+
 controlador.registrar= async (req,res)=>{
     const nuevocomercio = new comercio(req.body)
     console.log(nuevocomercio)
     await nuevocomercio.save();
     res.send("Se creo nuevo comercio")
 }
+
+controlador.eliminar= async (req,res)=>{
+    console.log("Eliminación individual")
+    await comercio.findByIdAndDelete(req.params.id)
+    res.json({"status":"Comercio eliminado"})
+}
+
+controlador.actualizar= async (req,res)=>{
+    console.log("Actualizando un comercio")
+    await comercio.findByIdAndUpdate(req.params.id,req.body)
+    res.json({"status":"Comercio actualizado"})
+}
+
+
 
 export default controlador
--- routes/comercio.routes.js
+++ routes/comercio.routes.js
@@ -1,9 +1,15 @@
 import Router from "express"
 import cController from "../controllers/comercioController.js"
 const ruta = Router();
-
+//todos
 ruta.get("/listadoComercios",cController.listado)
+//uno
+ruta.get("/uno/:id",cController.uno)
+
 ruta.post("/registrar",cController.registrar)
 
+ruta.delete("/eliminarComercio/:id",cController.eliminar)
+
+ruta.put("/editarComercio/:id",cController.actualizar)
 
 export default ruta
